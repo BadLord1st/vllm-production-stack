bijualbert/production-stack

commit a2f32bca047574b661eaeb80f21b509997f74bc0

[Feat] KV cache aware routing  (#403)
* Fix pyproject.toml

Signed-off-by: YuhanLiu11 <yliu738@wisc.edu>

* Add KV cache aware routing

Signed-off-by: YuhanLiu11 <yliu738@wisc.edu>

* Add lmcache in docs

Signed-off-by: YuhanLiu11 <yliu738@wisc.edu>

* Add lmcache in docs

Signed-off-by: YuhanLiu11 <yliu738@wisc.edu>

* fix import error

Signed-off-by: YuhanLiu11 <yliu738@wisc.edu>

* Add helm chart

Signed-off-by: YuhanLiu11 <yliu738@wisc.edu>

* Update router docker image

Signed-off-by: YuhanLiu11 <yliu738@wisc.edu>

* Adds comments and delete unused functions

Signed-off-by: YuhanLiu11 <yliu738@wisc.edu>

* not checking instance ip all the time

Signed-off-by: YuhanLiu11 <yliu738@wisc.edu>

---------

Signed-off-by: YuhanLiu11 <yliu738@wisc.edu>

src/vllm_router/services/request_service/request.py
@@ -22,6 +22,7 @@ from fastapi import BackgroundTasks, Request
 from fastapi.responses import JSONResponse, StreamingResponse
 
 from vllm_router.log import init_logger
+from vllm_router.routers.routing_logic import KvawareRouter
 from vllm_router.service_discovery import get_service_discovery
 from vllm_router.services.request_service.rewriter import (
     get_request_rewriter,
@@ -135,11 +136,10 @@ async def process_request(
         await store_in_semantic_cache(
             endpoint=endpoint, method=request.method, body=body, chunk=cache_chunk
         )
-
-    if hasattr(request.app.state, "callbacks"):
-        background_tasks.add_task(
-            request.app.state.callbacks.post_request, request, full_response
-        )
+        if background_tasks and hasattr(request.app.state, "callbacks"):
+            background_tasks.add_task(
+                request.app.state.callbacks.post_request, request, full_response
+            )
 
 
 async def route_general_request(
@@ -208,9 +208,14 @@ async def route_general_request(
         )
 
     logger.debug(f"Routing request {request_id} for model: {requested_model}")
-    server_url = request.app.state.router.route_request(
-        endpoints, engine_stats, request_stats, request
-    )
+    if isinstance(request.app.state.router, KvawareRouter):
+        server_url = await request.app.state.router.route_request(
+            endpoints, engine_stats, request_stats, request, request_json
+        )
+    else:
+        server_url = request.app.state.router.route_request(
+            endpoints, engine_stats, request_stats, request
+        )
     curr_time = time.time()
     logger.info(
         f"Routing request {request_id} to {server_url} at {curr_time}, process time = {curr_time - in_router_time:.4f}"

docker/Dockerfile
@@ -15,7 +15,7 @@ COPY .git/ .git/
 # Copy the rest of the application code
 COPY src/ src/
 
-ARG INSTALL_OPTIONAL_DEP=semantic_cache
+ARG INSTALL_OPTIONAL_DEP=semantic_cache,lmcache
 ENV INSTALL_OPTIONAL_DEP=${INSTALL_OPTIONAL_DEP}
 
 # Install dependencies (use cache, and delete after install, to speed up the build)

docs/source/conf.py
@@ -94,6 +94,7 @@ autodoc_mock_imports = [
     "kubernetes",
     "prometheus_client",
     "uhashring",
+    "lmcache",
 ]
 
 intersphinx_mapping = {

examples/kv_routing/lmcache1.yaml
@@ -1,0 +1,9 @@
+chunk_size: 256
+local_cpu: True
+max_local_cpu_size: 5
+
+# cache controller configurations
+enable_controller: True
+lmcache_instance_id: "default1"
+controller_url: "localhost:9001"
+lmcache_worker_port: 8002

examples/kv_routing/prod-stack.yaml
@@ -1,0 +1,43 @@
+servingEngineSpec:
+  runtimeClassName: ""
+  modelSpec:
+  - name: "mistral"
+    repository: "lmcache/test"
+    tag: "2025-05-04"
+    modelURL: "mistralai/Mistral-7B-Instruct-v0.2"
+    replicaCount: 1
+    requestCPU: 6
+    requestMemory: "30Gi"
+    requestGPU: 1
+    pvcStorage: "50Gi"
+    vllmConfig:
+      enableChunkedPrefill: false
+      enablePrefixCaching: false
+      maxModelLen: 16384
+
+    lmcacheConfig:
+      enabled: true
+      cpuOffloadingBufferSize: "20"
+      enableController: true
+      instanceId: "default1"
+      controllerPort: "9001"
+      workerPort: 8002
+
+    env:
+      - name: LMCACHE_LOG_LEVEL
+        value: "DEBUG"
+    hf_token: <YOUR_HF_TOKEN>
+
+
+routerSpec:
+  repository: "lmcache/lmstack-router"
+  tag: "v0.1.2"
+  resources:
+    requests:
+      cpu: "1"
+      memory: "2G"
+    limits:
+      cpu: "1"
+      memory: "2G"
+  routingLogic: "kvaware"
+  lmcacheControllerPort: 9001

helm/templates/deployment-router.yaml
@@ -96,6 +96,10 @@ spec:
           {{- if .Values.routerSpec.extraArgs }}
           {{- toYaml .Values.routerSpec.extraArgs | nindent 10 }}
           {{- end }}
+          {{- if .Values.routerSpec.lmcacheControllerPort }}
+          - "--lmcache-controller-port"
+          - "{{ .Values.routerSpec.lmcacheControllerPort }}"
+          {{- end }}
         {{- if .Values.routerSpec.resources }}
         resources:
           {{- if .Values.routerSpec.resources.requests }}

helm/templates/deployment-vllm-multi.yaml
@@ -213,6 +213,22 @@ spec:
           - name: LMCACHE_REMOTE_SERDE
             value: "{{ .Values.cacheserverSpec.serde }}"
           {{-   end }}
+          {{-   if hasKey $modelSpec.lmcacheConfig "enableController" }}
+          - name: LMCACHE_ENABLE_CONTROLLER
+            value: {{ ternary "True" "False" $modelSpec.lmcacheConfig.enableController | quote }}
+          {{-   end }}
+          {{-   if hasKey $modelSpec.lmcacheConfig "instanceId" }}
+          - name: LMCACHE_INSTANCE_ID
+            value: {{ $modelSpec.lmcacheConfig.instanceId | quote }}
+          {{-   end }}
+          {{-   if hasKey $modelSpec.lmcacheConfig "controllerPort" }}
+          - name: LMCACHE_CONTROLLER_URL
+            value: "{{ .Release.Name }}-{{$modelSpec.name}}-service:{{ $modelSpec.lmcacheConfig.controllerPort }}"
+          {{-   end }}
+          {{-   if hasKey $modelSpec.lmcacheConfig "workerPort" }}
+          - name: LMCACHE_WORKER_PORT
+            value: "{{ .Release.Name }}-service:{{ $modelSpec.lmcacheConfig.workerPort }}"
+          {{-   end }}
           {{- end }}
           {{- if .Values.servingEngineSpec.configs }}
           envFrom:

pyproject.toml
@@ -37,6 +37,9 @@ semantic_cache = [
     "faiss-cpu==1.10.0",
     "huggingface-hub==0.25.2",  # downgrade to 0.25.2 to avoid breaking changes
 ]
+lmcache = [
+    "lmcache==0.2.11",
+]
 
 [build-system]
 requires = ["setuptools>=68", "setuptools_scm[toml]>=8.0"]
@@ -53,5 +56,5 @@ lint = [
 ]
 test = [
     "pytest>=8.3.4",
-    "pytest-asyncio>=0.25.3",
+    "pytest-asyncio>=0.25.3"
 ]

BadLord1st/vllm-production-stack

commit 035f2e6182d2ad30c06c89fd04b6271776322c49

fix(request): make callbacks not require semantic_cache (#465)
Broke in https://github.com/vllm-project/production-stack/commit/a2f32bca047574b661eaeb80f21b509997f74bc0#diff-3e852cfcf107dfd78ee2b7c7e88e05428b7f2bffcfc5ca1dce0c070caf563f2aR139

Same as https://github.com/vllm-project/production-stack/pull/399

Signed-off-by: Max Wittig <max.wittig@siemens.com>

src/vllm_router/services/request_service/request.py
@@ -141,10 +141,10 @@ async def process_request(
         await store_in_semantic_cache(
             endpoint=endpoint, method=request.method, body=body, chunk=cache_chunk
         )
-        if background_tasks and hasattr(request.app.state, "callbacks"):
-            background_tasks.add_task(
-                request.app.state.callbacks.post_request, request, full_response
-            )
+    if background_tasks and hasattr(request.app.state, "callbacks"):
+        background_tasks.add_task(
+            request.app.state.callbacks.post_request, request, full_response
+        )
 
 
 async def route_general_request(